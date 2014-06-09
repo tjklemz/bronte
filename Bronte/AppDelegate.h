8f054016c440c76b8a35ddbaa8975a71fa0fb564
Thomas Klemz <tjklemz@Thomass-MacBook-Air.local>
2014-06-09 14:56:41 -0700

    Initial Commit

---
+++ Bronte/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Bronte
+//
+//  Created by Thomas Klemz on 6/9/14.
+//  Copyright (c) 2014 Lory & Ludlow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
