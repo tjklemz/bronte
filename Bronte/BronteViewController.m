d6b4a996b29ad0ceb04f937db9a63e5a1e3bbe90
Thomas Klemz <tjklemz@gmail.com>
2014-07-05 11:09:21 -0700

    spacing issues, minor

--- Bronte/BronteViewController.m
+++ Bronte/BronteViewController.m
@@ -91,7 +91,7 @@
         //testing only
         [self addText:@"You don't know about me without you have read a book by the name of The Adventures of Tom Sawyer; but that ain't no matter.  That book was made by Mr. Mark Twain, and he told the truth, mainly.  There was things which he stretched, but mainly he told the truth.  That is nothing.  I never seen anybody but lied one time or another, without it was Aunt Polly, or the widow, or maybe Mary.  Aunt Polly — Tom's Aunt Polly, she is — and Mary, and the Widow Douglas is all told about in that book, which is mostly a true book, with some stretchers, as I said before." toLine:_lines.lastObject];
         
-        for (int i = 0; i < 6; ++i) {
+        for (int i = 0; i < 1; ++i) {
             [self addText:@"Now the way that the book winds up is this:  Tom and me found the money that the robbers hid in the cave, and it made us rich.  We got six thousand dollars apiece — all gold.  It was an awful sight of money when it was piled up.  Well, Judge Thatcher he took it and put it out at interest, and it fetched us a dollar a day apiece all the year round — more than a body could tell what to do with.  The Widow Douglas she took me for her son, and allowed she would sivilize me; but it was rough living in the house all the time, considering how dismal regular and decent the widow was in all her ways; and so when I couldn't stand it no longer I lit out.  I got into my old rags and my sugar-hogshead again, and was free and satisfied.  But Tom Sawyer he hunted me up and said he was going to start a band of robbers, and I might join if I would go back to the widow and be respectable.  So I went back." toLine:_lines.lastObject];
             
             [self addText:@"The widow she cried over me, and called me a poor lost lamb, and she called me a lot of other names, too, but she never meant no harm by it. She put me in them new clothes again, and I couldn't do nothing but sweat and sweat, and feel all cramped up.  Well, then, the old thing commenced again.  The widow rung a bell for supper, and you had to come to time. When you got to the table you couldn't go right to eating, but you had to wait for the widow to tuck down her head and grumble a little over the victuals, though there warn't really anything the matter with them, — that is, nothing only everything was cooked by itself.  In a barrel of odds and ends it is different; things get mixed up, and the juice kind of swaps around, and the things go better." toLine:_lines.lastObject];
@@ -173,7 +173,7 @@
 }
 
 - (CGPoint)lineOriginForLineNumber:(NSUInteger)n {
-    float offset = n < _lines.count && [_lines[n] isParagraphSeparator] ? 50 : 0;
+    float offset = n < _lines.count && [_lines[n] isParagraphSeparator] ? 63 - 0.5*[NSNumber linePadding] : 0;
     return CGPointMake(([self width] - [UIFont bronteLineWidth])/2.0 - [NSNumber lineHandleWidth] + 15 + offset, 50 + n*[NSNumber lineHeight]);
 }
 
@@ -434,7 +434,7 @@
             int lineNo1 = [hitInfo1[@"lineNo"] intValue];
             int lineNo2 = [hitInfo2[@"lineNo"] intValue];
             
-            wordIcon.position = CGPointMake((w1.superlayer.bounds.size.width - w1.position.x) + 30, 0.5*(lineNo2 - lineNo1 + 0.675)*[NSNumber lineHeight]);
+            wordIcon.position = CGPointMake((w1.superlayer.bounds.size.width - w1.position.x - wordIcon.frame.size.width) + 30, 0.5*(lineNo2 - lineNo1 + 0.675)*[NSNumber lineHeight]);
             [w1 addSublayer:wordIcon];
             _selectionInfo[@"icon"] = wordIcon;
             
